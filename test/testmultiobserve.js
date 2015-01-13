83a7feb185f332adfc4ba60a885c252de7238d57
Krzysztof Kabat <odrinwhite@hotmail.com>
2015-01-13 22:31:34 +0000

    basic setup with simple test

---
+++ test/testmultiobserve.js
@@ -0,0 +1,39 @@
+var chai= require('chai')
+var spies = require('chai-spies')
+var multiobserve = require('../lib/multiobserve.js').multiobserve
+
+chai.use(spies);
+
+var should = chai.should()
+  , expect = chai.expect;
+
+describe("multiobserve", function() {
+    describe(".observe()", function() {
+        it("return correct path value and oldValue", function(done) {
+            var object = multiobserve.observe({
+                property: {
+                    a: 1,
+                    b: {
+                        c: 'hello'
+                    }
+                }
+            }, function(changes) {
+                changes.forEach(function(change) {
+                    expect(change.path).to.eql(['property', 'b', 'c'])
+                    expect(change.value).to.eql('bye')
+                    expect(change.oldValue).to.eql('hello')
+                    done()
+                })
+            })
+            object.property.b.c = 'bye'
+        })
+
+        it('should not call callback', function() {
+            var spyCallback = chai.spy()
+            
+            multiobserve.observe({}, spyCallback)
+            
+            expect(spyCallback).to.not.have.been.called()
+        })
+    })
+})
