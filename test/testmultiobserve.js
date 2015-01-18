429a669966bfa89af2a4031c2897917690894b02
Krzysztof Kabat <odrinwhite@hotmail.com>
2015-01-18 20:46:53 +0000

    first attempt to implement unobserve during delete property

--- test/testmultiobserve.js
+++ test/testmultiobserve.js
@@ -20,19 +20,19 @@ describe('multiobserve', function() {
 
             Object.observe(deep(object), function(changes) {
                 expect(changes[0]).to.eql({
-                    object : object,
-                    name : 'propX',
-                    type : 'update',
-                    oldValue : 10
+                    object: object,
+                    name: 'propX',
+                    type: 'update',
+                    oldValue: 10
                 })
 
                 expect(changes[1]).to.eql({
-                    object : object,
-                    name : 'propZ',
-                    type : 'update',
-                    node : object.propY,
-                    oldValue : 33,
-                    path : ['propY', 'propZ']
+                    object: object,
+                    name: 'propZ',
+                    type: 'update',
+                    node: object.propY,
+                    oldValue: 33,
+                    path: ['propY', 'propZ']
                 })
 
                 done()
@@ -49,12 +49,12 @@ describe('multiobserve', function() {
 
             Object.observe(deep(object), function(changes) {
                 expect(changes[0]).to.eql({
-                    object : object,
-                    name : 'propZ',
-                    type : 'add',
-                    node : object.propY,
-                    oldValue : undefined,
-                    path : ['propY', 'propZ']
+                    object: object,
+                    name: 'propZ',
+                    type: 'add',
+                    node: object.propY,
+                    oldValue: undefined,
+                    path: ['propY', 'propZ']
                 })
 
                 done()
@@ -72,43 +72,144 @@ describe('multiobserve', function() {
 
             Object.observe(deep(object), function(changes) {
                 expect(changes[0]).to.eql({
-                    object : object,
-                    name : 'propZ',
-                    type : 'delete',
-                    node : object.propY,
-                    oldValue : 33,
-                    path : ['propY', 'propZ']
+                    object: object,
+                    name: 'propZ',
+                    type: 'delete',
+                    node: object.propY,
+                    oldValue: 33,
+                    path: ['propY', 'propZ']
                 })
 
                 done()
             })
             delete object.propY.propZ
         })
-        
+
         it('should call callback change releated with update array element', function(done) {
             var object = {
                 propX: 10,
                 propY: {
-                    propZ: [1,2,3,4]
+                    propZ: [1, 2, 3, 4]
                 }
             }
 
             Object.observe(deep(object), function(changes) {
                 expect(changes[0]).to.eql({
-                    object : object,
-                    name : '2',
-                    type : 'update',
-                    node : object.propY.propZ,
-                    oldValue : 3,
-                    path : ['propY', 'propZ', '2'],
-                    index : undefined,
+                    object: object,
+                    name: '2',
+                    type: 'update',
+                    node: object.propY.propZ,
+                    oldValue: 3,
+                    path: ['propY', 'propZ', '2'],
+                    index: undefined,
                     removed: undefined,
-                    addedCount : undefined
+                    addedCount: undefined
                 })
-                
+
                 done()
             })
             object.propY.propZ[2] = 99
+        })
+
+        it('should call callback change when adding property and changing its property', function(done) {
+            var object = {
+                propX: 10,
+                propY: {}
+            }
+            var callTimes = 0;
+
+            Object.observe(deep(object), function(changes) {
+                callTimes++;
+                if (callTimes === 1) {
+                    expect(changes[0]).to.eql({
+                        object: object,
+                        name: 'propZ',
+                        type: 'add',
+                        node: {
+                            propZ: {
+                                propN: {
+                                    propP: 20
+                                }
+                            }
+                        },
+                        oldValue: undefined,
+                        path: ['propY', 'propZ']
+                    })
+                    setTimeout(function() {
+                        object.propY.propZ.propN.propP = 65
+                    }, 0);
+                } else {
+                    expect(changes[0]).to.eql({
+                        object: object,
+                        name: 'propP',
+                        type: 'update',
+                        node: object.propY.propZ.propN,
+                        oldValue: 20,
+                        path: ['propY', 'propZ', 'propN', 'propP']
+                    })
+                    done()
+                }
+            })
+            object.propY.propZ = {
+                propN: {
+                    propP: 10
+                }
+            }
+            object.propY.propZ.propN.propP = 20
+        })
+
+        it('should not call callback when removing property and later changing removed part property', function(done) {
+            var object = {
+                propX: 10,
+                propY: {
+                    propZ: {
+                        propN: {
+                            propP: 10
+                        }
+                    }
+                }
+            }
+            var tmpPointer = object.propY.propZ
+            var callTimes = 0;
+
+            function changeFunction(changes) {
+                callTimes++;
+                if (callTimes === 1) {
+                    expect(changes[0]).to.eql({
+                        object: object,
+                        name: 'propP',
+                        type: 'update',
+                        node: object.propY.propZ.propN,
+                        oldValue: 10,
+                        path: ['propY', 'propZ', 'propN', 'propP']
+                    })
+
+                    setTimeout(function() {
+                        delete object.propY.propZ
+                    }, 0)
+                } else {
+                    expect(changes[0]).to.eql({
+                        object: object,
+                        name: 'propZ',
+                        type: 'delete',
+                        node: {},
+                        oldValue: tmpPointer,
+                        path: ['propY', 'propZ']
+                    })
+                    tmpPointer.propN.propP = 32
+
+                    setTimeout(function() {
+                        expect(changeFunctionSpy).to.have.been.called.exactly(2)
+                        done()
+                    }, 0)
+                }
+            }
+
+            var changeFunctionSpy = chai.spy(changeFunction)
+
+            Object.observe(deep(object), changeFunctionSpy)
+            object.propY.propZ.propN.propP = 69
+
         })
     })
 })
