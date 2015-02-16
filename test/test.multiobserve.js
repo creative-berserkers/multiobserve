b50d3217fe393d6c07a19602128730e14809ba18
odrinwhite <odrinwhite@gmail.com>
2015-02-16 05:00:24 +0100

    fixed tests

--- test/test.multiobserve.js
+++ test/test.multiobserve.js
@@ -137,6 +137,7 @@ describe('Multiobserve', function() {
             }
             let tmpPointer = object.propY.propZ
             let callTimes = 0;
+            let changeFunctionSpy
 
             function changeFunction(changes) {
                 callTimes++;
@@ -145,7 +146,7 @@ describe('Multiobserve', function() {
                         node: object.propY.propZ.propN,
                         path: ['propY', 'propZ', 'propN', 'propP'],
                         type: 'update',
-                        oldValue: 10,
+                        oldValue: 10
                     })
 
                     setTimeout(function() {
@@ -156,7 +157,7 @@ describe('Multiobserve', function() {
                         node: {},
                         path: ['propY', 'propZ'],
                         type: 'delete',
-                        oldValue: tmpPointer,
+                        oldValue: tmpPointer
                     })
                     tmpPointer.propN.propP = 32
 
@@ -167,7 +168,7 @@ describe('Multiobserve', function() {
                 }
             }
 
-            let changeFunctionSpy = chai.spy(changeFunction)
+            changeFunctionSpy = chai.spy(changeFunction)
 
             Multiobserve.observe(object, changeFunctionSpy)
             object.propY.propZ.propN.propP = 69
@@ -219,7 +220,7 @@ describe('Multiobserve', function() {
             object.propY.pop()
         })
 
-        it('should call callback change releated with update array element', function(done) {
+        it('should call callback change related with update array element', function(done) {
             let object = {
                 propX: 10,
                 propY: {
@@ -240,7 +241,7 @@ describe('Multiobserve', function() {
             object.propY.propZ[2] = 99
         })
         
-        it('should call callback change releated with calling splice on array element', function(done) {
+        it('should call callback change related with calling splice on array element', function(done) {
             let object = {
                 propX: 10,
                 propY: {
@@ -277,7 +278,7 @@ describe('Multiobserve', function() {
             
         })
         
-        it('should call callback change releated with update element within array', function(done) {
+        it('should call callback change related with update element within array', function(done) {
             let object = {
                 propX: 10,
                 propY: {
@@ -289,7 +290,6 @@ describe('Multiobserve', function() {
                 }
             }
 
-            let callTimes = 0;
             Multiobserve.observe(object, function(changes) {
                 expect(changes[0]).to.eql({
                     node: object,
@@ -301,7 +301,7 @@ describe('Multiobserve', function() {
                     node: object.propY.propZ[1],
                     path: ['propY', 'propZ', '1', 'propN'],
                     type: 'update',
-                    oldValue: 11,
+                    oldValue: 11
                 })
 
                 done()
@@ -343,57 +343,6 @@ describe('Multiobserve', function() {
                         node: object.propY.propZ[0],
                         path: ['propY', 'propZ', '0', 'propN'],
                         type: 'update',
-                        oldValue: 11,
-                    })
-    
-                    done()
-                }
-                callTimes++
-            })
-            object.propX = 34
-            object.propY.propZ.push({
-                propN : 11
-            })
-            
-        })
-        
-        it('should call callback change releated with update element within array after push when array contained one element', function(done) {
-            let object = {
-                propX: 10,
-                propY: {
-                    propZ: [{
-                        propN : 89
-                    }]
-                }
-            }
-
-            let callTimes = 0;
-            Multiobserve.observe(object, function(changes) {
-                console.log(changes)
-                console.log(changes)
-                if(callTimes === 0){
-                    expect(changes[0]).to.eql({
-                        node: object,
-                        name: ['propX'],
-                        type: 'update',
-                        oldValue: 10
-                    })
-                    expect(changes[1]).to.eql({ 
-                        node: object.propY.propZ,
-                        path: [ 'propY' , 'propZ'],
-                        type: 'splice',
-                        index: 1,
-                        removed: [],
-                        addedCount: 1
-                    })
-                    setTimeout(function() {
-                        object.propY.propZ[1].propN = 12
-                    }, 0);
-                } else {
-                    expect(changes[0]).to.eql({
-                        node: object.propY.propZ[1],
-                        path: ['propY', 'propZ', '1', 'propN'],
-                        type: 'update',
                         oldValue: 11
                     })
     
@@ -407,6 +356,173 @@ describe('Multiobserve', function() {
             })
             
         })
+        
+        it('should call callback change related with update element within array after push when array contained one element', function(done) {
+            let object = {
+                propX: 10,
+                propY: {
+                    propZ: [{
+                        propN : 89
+                    }]
+                }
+            }
 
+            let callTimes = 0;
+            var tmp
+            Multiobserve.observe(object, function(changes) {
+                if(callTimes === 0){
+                    expect(changes[0]).to.eql({
+                        node: object.propY.propZ,
+                        path: [ 'propY' , 'propZ'],
+                        type: 'splice',
+                        index: 1,
+                        removed: [],
+                        addedCount: 1
+                    })
+                    setTimeout(function() {
+                        object.propY.propZ[0].propN = 1111
+                        object.propY.propZ[1].propN = 2222
+                    }, 0);
+                } else if(callTimes === 1){
+                    expect(changes[0]).to.eql({
+                        node: object.propY.propZ[0],
+                        path: ['propY', 'propZ', '0', 'propN'],
+                        type: 'update',
+                        oldValue: 89
+                    })
+                    expect(changes[1]).to.eql({
+                        node: object.propY.propZ[1],
+                        path: ['propY', 'propZ', '1', 'propN'],
+                        type: 'update',
+                        oldValue: 11
+                    })
+                    setTimeout(function() {
+                        tmp = object.propY.propZ[0]
+                        object.propY.propZ.splice(0,1,{
+                            propN : 3333
+                        })
+                    }, 0);
+    
+
+                } else if(callTimes === 2){
+                    expect(changes[0]).to.eql({
+                        node: object.propY.propZ,
+                        path: [ 'propY' , 'propZ'],
+                        type: 'splice',
+                        index: 0,
+                        removed: [{
+                            propN: 1111
+                        }],
+                        addedCount: 1
+                    })
+                    setTimeout(function() {
+                        tmp.propN = 4444
+                        object.propY.propZ[1].propN = 6666 //old 2222
+                        object.propY.propZ[0].propN = 5555 //old 3333
+                    }, 0);
+
+                } else {
+                    expect(changes[0]).to.eql({
+                        node: object.propY.propZ[1],
+                        path: ['propY', 'propZ', '1', 'propN'],
+                        type: 'update',
+                        oldValue: 2222
+                    })
+                    expect(changes[1]).to.eql({
+                        node: object.propY.propZ[0],
+                        path: ['propY', 'propZ', '0', 'propN'],
+                        type: 'update',
+                        oldValue: 3333
+                    })
+
+                    done()
+                }
+                callTimes++
+            })
+            object.propY.propZ.push({
+                propN : 11
+            })
+            
+        })
+    })
+
+    describe('.observe() - property filter', function() {
+        it('should call filter callback for each object function and array in object tree, also for added ones', function(done) {
+            let object = {
+                propX: {
+                    propK: 'test'
+                },
+                propY: {
+                    propZ: [{
+                        propN: {name: 'john'},
+                        propM: function () {
+                        }
+                    }]
+                }
+            }
+            let callTimes = 0;
+            Multiobserve.observe(object, function () {
+            }, function (node, path) {
+                if (callTimes === 0) {
+                    expect(node).to.eql(object.propX)
+                    expect(path).to.eql(['propX'])
+                } else if (callTimes === 1) {
+                    expect(node).to.eql(object.propY)
+                    expect(path).to.eql(['propY'])
+                } else if (callTimes === 2) {
+                    expect(node).to.eql(object.propY.propZ)
+                    expect(path).to.eql(['propY', 'propZ'])
+                } else if (callTimes === 3) {
+                    expect(node).to.eql(object.propY.propZ[0])
+                    expect(path).to.eql(['propY', 'propZ', '0'])
+                } else if (callTimes === 4) {
+                    expect(node).to.eql(object.propY.propZ[0].propN)
+                    expect(path).to.eql(['propY', 'propZ', '0', 'propN'])
+                } else if (callTimes === 5) {
+                    expect(node).to.eql(object.propY.propZ[0].propM)
+                    expect(path).to.eql(['propY', 'propZ', '0', 'propM'])
+                } else if (callTimes === 6) {
+                    expect(node).to.eql(object.propY.propZ[1])
+                    expect(path).to.eql(['propY', 'propZ', '1'])
+                } else if (callTimes === 7) {
+                    expect(node).to.eql(object.propY.propZ[1].propN)
+                    expect(path).to.eql(['propY', 'propZ', '1', 'propN'])
+                } else if (callTimes === 8) {
+                    expect(node).to.eql(object.propY.propZ[1].propM)
+                    expect(path).to.eql(['propY', 'propZ', '1', 'propM'])
+                    done()
+                }
+                callTimes++
+                return true
+            })
+
+            object.propX = {
+                propK : 'hello'
+            }
+            object.propY.propZ.push({
+                propN: {name: 'patric'},
+                propM: function () {
+                }
+            })
+        })
+    })
+
+    describe('.observe() - custom modifier', function() {
+        let object = {}
+
+        it('should create swap modifier', function(done) {
+            function swap(object, node, prop1, prop2){
+                Multiobserve.performChange(object, function(notify){
+                    let tmp = node[prop1]
+                    node[prop1] = node[prop2]
+                    node[prop2] = tmp
+
+                    notify({
+                        changeName : 'swap'
+                    })
+                })
+            }
+            done()
+        })
     })
 })
